8f863d173448cdecac6e25e0586ee822179d9757
cosmi <cosmicy@outlook.com>
2015-12-07 16:20:55 +0800

    +添加C32_UIWidgetTest

---
+++ C32_UIWidgetTest/app/src/test/java/com/cosmicy/cosmi/c32_uiwidgettest/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.cosmicy.cosmi.c32_uiwidgettest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
